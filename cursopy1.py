bda3e289c22c54b0f2e7effda94b53e3aeeee9d8
Júlia Papa <65194855+juliapapa@users.noreply.github.com>
2021-07-15 21:20:07 -0300

    Add files via upload

---
+++ cursopy1.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 15 21:19:16 2021
+
+@author: julia
+"""
+
+# -*- coding: utf-8 -*-
+print("hello world!")
+print("outra mensagem")
+print('mensagem 3')
+
+# comentarios
+
+# operações matemáticas
+print(2+2)
+
+# potenciação
+print(2**3)
+# divisão
+print(10/3)
+# resto da divisão
+print(10 % 3)
+
+# Variáveis
+
+minha_variavel = 'hello world'
+print(minha_variavel)
+
+# tipos de variaveis
+# numerico inteiro
+# numerico flutuante
+# textual (string)
+# booleana
+
+var1 = 1
+var2 = 1.1
+var3 = 'uma string'
+var4 = True
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+
+# operadores logicos e relacionais
+
+x = 2
+y = 3
+z = 2 
+print(x == y)
+print(x > y)
+
+soma = x + y
+
+print (soma > x)
+
+# operadores logicos
+# condicionais
+# and; or; not
+
+print(x == y and x == soma)
+print(x == z)
+print(x == z or x == y)
+
+# comandos condicionais
+
+## if
+# if condição:
+    # execute essa linha
+    
+x = 1
+y = 1000
+
+if x > y:
+    print('x é maior que y')
+    
+if y > x:
+    print("y é maior que x") 
+    
